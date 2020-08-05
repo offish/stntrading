3e866fb5863e45f4c599e59974fad179180d2ed8
offish <overutilization@gmail.com>
2020-08-05 15:42:25 +0200

    Create __init__.py

---
+++ stntrading/__init__.py
@@ -0,0 +1,13 @@
+"""
+stntrading
+==========
+"""
+
+__title__ = 'stntrading'
+__author__ = 'offish'
+__license__ = 'MIT'
+__version__ = '1.0.0'
+
+
+from .enums import EStatusCodes
+from .stn import STN
